d0d6e4c1c1a596fc389fdab53d929d4de480a707
Peter Keating <peter@moov2.com>
2020-01-22 10:54:11 +0000

    Bump to 0.6.2

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "Useful Fields",
     Author = "Etch UK",
     Website = "https://etchuk.com",
-    Version = "0.6.1"
+    Version = "0.6.2"
 )]
 [assembly: Feature(
     Id = "Etch.OrchardCore.Fields.ResponsiveMedia",
